68acd3466657979c198bb96c22a65381d99273be
Vladimir <intfloatbool@gmail.com>
2020-03-12 11:11:36 +0300

    FX assets

--- Assets/Scripts/GlobalMap/LootGenerator.cs
+++ Assets/Scripts/GlobalMap/LootGenerator.cs
@@ -6,16 +6,14 @@ using UnityEngine;
 public class LootGenerator : MonoBehaviour
 {
     [SerializeField] private List<MapResource> _mapResourcePrefabs;
-    private Dictionary<ItemName, MapResource> _mapResourceDict = new Dictionary<ItemName, MapResource>();
-
     [SerializeField] private List<MapResource> _currentResources;
-
     [SerializeField] private int _minResourcesCount = 5;
     [SerializeField] private int _maxResourcesCount = 15;
-    
     [SerializeField] private GOMap _goMap;
     private GOTile _lastGoTile;
 
+    [SerializeField] private Transform _lootParent;
+    
     private void Awake()
     {
         _goMap.OnTileLoad.AddListener(OnTileLoad);
@@ -29,42 +27,38 @@ public class LootGenerator : MonoBehaviour
         }
 
         GenerateResources(tile);
-
         _lastGoTile = tile;
-        Debug.Log("LOOT GENERATOR: TILE LOADING! : " + tile.gameObject.name);
     }
 
     private MapResource CreateRandomResource()
     {
         var prefab = _mapResourcePrefabs[Random.Range(0, _mapResourcePrefabs.Count)];
-
-
-        var resourceInstance = Instantiate(prefab);
+        var resourceInstance = Instantiate(prefab, _lootParent);
         return resourceInstance;
 
     }
 
     private void GenerateResources(GOTile goTile)
-    {
-        
-        var tileMeshFilter = goTile.GetComponent<MeshFilter>();
-        if (tileMeshFilter == null)
-        {
-            return;
-        }
-
-        var vertices = tileMeshFilter.mesh.vertices;
-        var rndCount = Random.Range(_minResourcesCount, _maxResourcesCount);
+    {          
+        var rndCount = Random.Range(_minResourcesCount, _maxResourcesCount);     
+        UpdateResources();
+        _lootParent = goTile.transform;
         for (int i = 0; i < rndCount; i++)
         {
-            var randomVertice = vertices[Random.Range(0, vertices.Length)];
-            var worldPos = gameObject.transform.TransformPoint(randomVertice);          
-            var resource = CreateRandomResource();
-            resource.transform.position = worldPos;
-
-            _currentResources.Add(resource);
+            var randomPoint = MapHelpers.GetRandomWorldPointFromGoTile(goTile);
+            if(!MapHelpers.IsWater(randomPoint))
+            {
+                var resource = CreateRandomResource();
+                resource.transform.position = randomPoint;
+                _currentResources.Add(resource);
+            }
+            
         }
 
-        var col = GetComponent<MeshCollider>();
+    }
+
+    private void UpdateResources()
+    {
+        _currentResources.RemoveAll(r => r == null);
     }
 }
---
+++ Assets/Scripts/GlobalMap/MapHelpers.cs
@@ -0,0 +1,38 @@
+﻿using GoMap;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MapHelpers 
+{
+    private static readonly string waterName = "water";
+    public static bool IsWater(Vector3 position)
+    {
+        var radius = 1f;
+        var colliders = Physics.OverlapSphere(position, radius);
+        foreach (var collider in colliders)
+        {
+            if (collider.transform.parent.name.Equals(waterName))
+                return true;
+        }
+        return false;
+    }
+
+    public static Vector3 GetRandomWorldPointFromGoTile(GOTile goTile)
+    {
+        var tileMeshFilter = goTile?.GetComponent<MeshFilter>();
+        if (tileMeshFilter == null)
+        {
+            Debug.LogError("Go tile not contains meshFilter!");
+            return Vector3.zero;
+        }
+
+        var vertices = tileMeshFilter.mesh.vertices;
+        var localToWorld = goTile.transform.localToWorldMatrix;
+
+        var randomVertice = vertices[Random.Range(0, vertices.Length)];
+        var worldPos = localToWorld.MultiplyPoint3x4(randomVertice);
+
+        return worldPos;
+    }
+}
