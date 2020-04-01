6cc44cee63556e28f8052df15d3071b315919f68
intfloatbool <intfloatbool@gmail.com>
2020-04-01 19:31:55 +0300

    Item craft base

---
+++ Assets/Scripts/Items/Craft/ItemCraftSystem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Items
+{
+    public class ItemCraftSystem : UnitySingletonBase<ItemCraftSystem>
+    {
+        protected override ItemCraftSystem GetInstance() => this;
+
+        public ItemName? TryCraftItem(params ItemName[] names)
+        {
+            //TODO: Realize craft logic
+            return null;
+        }
+        
+    }
+}
