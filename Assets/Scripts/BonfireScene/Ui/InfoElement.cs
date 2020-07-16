34a1ec21cf3b84e2ec9daaf1a044222df3d50cf0
Vladimir Khanyakin <vladimir.khanyakin@mytona.com>
2020-07-16 21:18:32 +0300

    new icons

---
+++ Assets/Scripts/BonfireScene/Ui/InfoElement.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NunclearGame.BonfireSceneUI
+{
+    public class InfoElement : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _titleText;
+        [SerializeField] private TextMeshProUGUI _valueText;
+        [SerializeField] private Image _iconImg;
+        public void Init(string titleContent, string valueContent, Sprite iconSprite = null)
+        {
+            if (_titleText != null)
+            {
+                _titleText.text = titleContent;
+            }
+
+            if (_valueText != null)
+            {
+                _valueText.text = valueContent;
+            }
+
+            if (_iconImg != null)
+            {
+                if (iconSprite != null)
+                {
+                    _iconImg.sprite = iconSprite;
+                }
+            }
+        }
+    }
+}
