355e0189bf00d46b3869c9badb9d9bd9b27aab27
Vladimir Khanyakin <vladimir.khanyakin@mytona.com>
2020-07-16 17:06:16 +0300

    new helpers

---
+++ Assets/Scripts/UnityCommonHelpers/TransformFollower.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DrawMagicGame.Prototype
+{
+    public class TransformFollower : MonoBehaviour
+    {
+        [SerializeField] private Transform _followTransform;
+
+        public Transform FollowTransform
+        {
+            get { return _followTransform; }
+            set { this._followTransform = value; }
+        }
+        
+        [SerializeField] private float _moveSpeed = 6f;
+        [SerializeField] private float _rotSpeed = 6f;
+
+        private void Update()
+        {
+            if (_followTransform == null)
+                return;
+
+            transform.position =
+                Vector3.Lerp(transform.position, _followTransform.position, _moveSpeed * Time.deltaTime);
+            transform.rotation =
+                Quaternion.Lerp(transform.rotation, _followTransform.rotation, _rotSpeed * Time.deltaTime);
+        }
+    }
+}
